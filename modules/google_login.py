c165854dfd5e1f81c0c7432f92238d095496df16
Mule129 <mule01129@gmail.com>
2023-10-08 23:40:09 +0900

    [Feat] #3 - Add Util files

--- modules/google_login.py
+++ modules/google_login.py
@@ -2,13 +2,16 @@
 import os
 from fastapi.responses import JSONResponse
 
-import uvicorn
+
 from fastapi import FastAPI, Request
 from fastapi_sso.sso.google import GoogleSSO
 
+from utils.getConfig import get_config
+
+
 os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-CLIENT_ID =
-CLIENT_SECRET =
+CLIENT_ID = get_config().get("Google Client Token", "CLIENT_ID")
+CLIENT_SECRET = get_config().get("Google Client Token", "CLIENT_SECRET")
 
 app = FastAPI()
 
@@ -39,5 +42,3 @@ async def auth_callback(request: Request):
 
     return JSONResponse(content=data, headers={"Content-Type": "application/json; charset=UTF-8"})
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
